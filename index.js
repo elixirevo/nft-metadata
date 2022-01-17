9e8352635c401d0fb271212ebc107f96780d1e80
elixirevo <elixirevocation@gmail.com>
2022-01-17 15:08:01 +0900

    update: end

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const baseData = require('./test-metadata.json')
+const fs = require('fs')
 
-function genMetadata(name, imgURL, url) {
+const baseData = require('./randomed-metadata.json')
+
+let imghashdata = []
+let metadata = []
+function genMetadata(
+  name,
+  imgURL,
+  homepage,
+  background,
+  body,
+  clothes,
+  eyes,
+  head,
+  mouth,
+  accessory
+) {
   return {
     name: name,
     image: imgURL,
-    external_url: url,
+    external_url: homepage,
     attributes: [
-      {
-        "trait_type": "Rarity",
-        "value": 0,
-        "max_value": 10000
-      },
       {
         trait_type: 'Background',
         value: background,
@@ -43,4 +53,42 @@ function genMetadata(name, imgURL, url) {
   }
 }
 
-console.log(baseData)
+for (let i = 0; i < baseData.length; i++) {
+  let name = `NFT #${i + 1}`
+  let imgURL = `https://url.mypinata.cloud/ipfs/${imghashdata[i]}`
+  let external_url = 'https://url.io'
+  let background = baseData[i].background
+  let body = baseData[i].body
+  let clothes = baseData[i].clothes
+  let eyes = baseData[i].eyes
+  let head = baseData[i].head
+  let mouth = baseData[i].mouth
+  let accessory = baseData[i].accessory
+  let data = genMetadata(
+    name,
+    imgURL,
+    external_url,
+    background,
+    body,
+    clothes,
+    eyes,
+    head,
+    mouth,
+    accessory
+  )
+  metadata.push(data)
+}
+
+const saveData = (data) => {
+  const finished = (error) => {
+    if (error) {
+      console.error(error)
+      return
+    }
+  }
+
+  const jsonData = JSON.stringify(data, null, 2)
+  fs.writeFile('final-metadata.json', jsonData, finished)
+}
+
+saveData(metadata)
